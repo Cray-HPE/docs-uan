e21720fb8f9649800dbee4459e46397b3de6b9c5
Jeff Keopp <77171187+keopp-hpe@users.noreply.github.com>
2022-12-13 15:47:56 -0600

    Generated HTML from docs-uan

---
+++ index.search.js
@@ -0,0 +1,16 @@
+var relearn_search_index = [
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Categories",
+    "uri": "/docs-uan/categories/index.html"
+  },
+  {
+    "content": "",
+    "description": "",
+    "tags": null,
+    "title": "Tags",
+    "uri": "/docs-uan/tags/index.html"
+  }
+]
