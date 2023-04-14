59351ffb468b69a8863e7d017dac8d11a5a06f07
Darrenn Jackson <darrenn.jackson@hpe.com>
2023-04-14 09:28:45 -0500

    committed updated python script

--- docs/hugo/docs-uan/bin/gen-hugo-toc-json.py
+++ docs/hugo/docs-uan/bin/gen-hugo-toc-json.py
@@ -1,9 +1,11 @@
 import json
 import os
 import sys
+import xml.etree.ElementTree as ET
 
 
-source_dir = sys.argv[1]
+#source_dir = sys.argv[1]
+source_dir = "../../../portal/developer-portal/"
 
 
 def gen_dir_toc(doc_dir):
@@ -24,31 +26,80 @@ def extract_title(md_file):
     return(md_title)
 
 
-with os.scandir(source_dir) as dir_item:
-    output_json = []
-    top_titles_fp = open(source_dir + "/top_titles.json", "r")
-    top_titles = json.load(top_titles_fp)
-    for title in top_titles:
+output_json = []
+ig_include = []
+top_titles_fp = open(source_dir + "top_titles.json", "r")
+top_titles = json.load(top_titles_fp)
+for title in top_titles:
+    if "title" in title:
         output_json.append(title)
-    top_titles_fp.close()
-    for entry in dir_item:
-        hugo_json_path = os.path.join(entry, "hugo_toc.json")
-        if entry.is_dir() and os.path.exists(hugo_json_path):
-            toc_fp = open(hugo_json_path)
-            toc = json.load(toc_fp)
-            toc_block = gen_dir_toc(entry)
-            toc["toc"] = toc_block
-            output_json.append(toc)
-            weight = toc["weight"]
-            for file in os.scandir(entry):
-                if file.is_file() and file.name.split(".")[-1] == "md":
-                    weight = weight + 1
-                    full_md_path = os.path.join(entry, file.name)
-                    md_title = extract_title(full_md_path)
-                    output_json.append({"title": md_title, "weight": weight})
-            toc_fp.close()
+    if "ig_include" in title:
+        ig_include.append(title["ig_include"])
+top_titles_fp.close()
+weight = 10
+with open(source_dir + "uan_install_guide.ditamap", 'r+', encoding="utf-8") as install_map:
+    tree = ET.parse(install_map)
+    install_map_root = tree.getroot()
+    for child in install_map_root:
+        if child.tag == "topicref":
+            topic_ref_path = child.get("href")
+            topic_title = extract_title(source_dir + topic_ref_path)
+            if ig_include.count(topic_title) == 1:
+                weight = weight + 10
+                output_json.append({"title": topic_title, "weight": weight})
+                topic_weight = 0
+                for grandchild in child.findall("./topicref"):
+                    topic_weight = topic_weight + 1
+                    topic_ref_path = grandchild.get("href")
+                    topic_title = extract_title(source_dir + topic_ref_path)
+                    output_json.append({"title": topic_title, "weight": weight + topic_weight})
+        if child.tag == "topichead" or child.tag == "topicset":
+            group_navtitle = child.find("./topicmeta/navtitle")
+            group_title = group_navtitle.text
+            if ig_include.count(topic_title) == 1:
+                weight = weight + 10
+                output_json.append({"title": group_title, "weight": weight + topic_weight})
+                topic_weight = 0
+                for grandchild in child.findall("./topicref"):
+                    topic_weight = topic_weight + 1
+                    topic_ref_path = grandchild.get("href")
+                    topic_title = extract_title(source_dir + topic_ref_path)
+                    output_json.append({"title": topic_title, "weight": weight + topic_weight})
     output_json_string = json.dumps(output_json, indent=2, separators=(',', ':'))
-    #print(output_json_string)
-    output_json_file = open(source_dir + "/hugo_toc.json", "w")
-    output_json_file.write(output_json_string)
-    output_json_file.close()
+    print(output_json_string)
+##    output_json_file = open(source_dir + "/hugo_toc.json", "w")
+##    output_json_file.write(output_json_string)
+##    output_json_file.close()
+
+
+    
+
+
+def old_main():
+   with os.scandir(source_dir) as dir_item:
+        output_json = []
+        top_titles_fp = open(source_dir + "/top_titles.json", "r")
+        top_titles = json.load(top_titles_fp)
+        for title in top_titles:
+            output_json.append(title)
+        top_titles_fp.close()
+        for entry in dir_item:
+            hugo_json_path = os.path.join(entry, "hugo_toc.json")
+            if entry.is_dir() and os.path.exists(hugo_json_path):
+                toc_fp = open(hugo_json_path)
+                toc = json.load(toc_fp)
+                toc_block = gen_dir_toc(entry)
+                toc["toc"] = toc_block
+                output_json.append(toc)
+                weight = toc["weight"]
+                for file in os.scandir(entry):
+                    if file.is_file() and file.name.split(".")[-1] == "md":
+                        weight = weight + 1
+                        full_md_path = os.path.join(entry, file.name)
+                        md_title = extract_title(full_md_path)
+                        output_json.append({"title": md_title, "weight": weight})
+                toc_fp.close()
+        output_json_string = json.dumps(output_json, indent=2, separators=(',', ':'))
+        output_json_file = open(source_dir + "/hugo_toc.json", "w")
+        output_json_file.write(output_json_string)
+        output_json_file.close()
