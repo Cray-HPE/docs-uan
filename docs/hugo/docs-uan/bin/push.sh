8b9e213b2600e12908bcaf2b86639ea7b74fd2b1
Jeff Keopp <77171187+keopp-hpe@users.noreply.github.com>
2023-06-08 19:04:32 -0500

    Update push.sh

--- docs/hugo/docs-uan/bin/push.sh
+++ docs/hugo/docs-uan/bin/push.sh
@@ -4,9 +4,7 @@ set -ex
 THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 [[ -d docs-uan ]] && rm -rf docs-uan || echo "docs-uan doesn't exist"
 mkdir -p docs-uan
-cd docs-uan
-git clone --no-checkout --depth=1 -b release/docs-html https://github.com/Cray-HPE/docs-uan.git 
-cd ..
+git clone --no-checkout --depth=1 -b release/docs-html https://github.com/Cray-HPE/docs-uan.git docs-uan
 rm -rf docs-uan/docs-uan/* docs-uan/docs-uan/.gitignore
 cp -r $THIS_DIR/../public/* docs-uan/docs-uan/
 cd docs-uan/docs-uan
