d63216e1aed78aadc687e9062778547bc2a7d057
Jeff Keopp <77171187+keopp-hpe@users.noreply.github.com>
2023-06-13 15:39:00 -0500

    Update push.sh

--- docs/hugo/docs-uan/bin/push.sh
+++ docs/hugo/docs-uan/bin/push.sh
@@ -8,14 +8,11 @@ pwd
 mkdir -p docs-uan
 cd docs-uan
 pwd
+git config --global user.name 'git'
 git clone --depth=1 -b release/docs-html https://github.com/Cray-HPE/docs-uan.git docs-uan
-ls -laR
 rm -rf docs-uan/* docs-uan/.gitignore
-ls -laR
 cp -r $THIS_DIR/../public/* docs-uan
 cd docs-uan
-ls -la
-git config --global user.name 'git@github.com'
 git add .
 git commit --amend --no-edit
 git push --force origin release/docs-html
